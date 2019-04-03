bdf8767eaa63cd7cdb7cbca75eebdadf0d100825
oleonort <oleonortt@gmail.com>
2019-04-03 21:57:42 +0200

    notes-app - working with command arguments

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,56 @@
 const chalk = require('chalk');
+const yargs = require('yargs');
 const { getNotes } = require('./notes');
 
-console.log(getNotes());
-console.log(chalk.red.inverse('Red!!'));
+// add, remove, read, list
+
+// Create add command
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  builder: {
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string'
+    },
+    body: {
+      describe: 'Note body',
+      demandOption: true,
+      type: 'string'
+    }
+  },
+  handler({ title, body }) {
+    console.log(title);
+    console.log(body);
+  }
+});
+
+// Create remove command
+yargs.command({
+  command: 'remove',
+  describe: 'Remove a note',
+  handler() {
+   console.log('Removing note..')
+  }
+});
+
+// Create read command
+yargs.command({
+  command: 'read',
+  describe: 'Read a note',
+  handler() {
+    console.log('Reading note..')
+  }
+});
+
+// Create read command
+yargs.command({
+  command: 'list',
+  describe: 'List your notes',
+  handler() {
+    console.log('Listing out all notes..')
+  }
+});
+
+yargs.parse();
